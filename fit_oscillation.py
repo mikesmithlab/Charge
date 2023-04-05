5daf52a760e69736968a6c3777dc8ab694d84944
mikesmithlab <mikesmithlab@gmail.com>
2023-04-05 12:24:14 +0100

    inertia_calc

--- fit_oscillation.py
+++ fit_oscillation.py
@@ -4,14 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-pathname = 'C:\\Users\\ppzmis\\Documents\\'
+pathname = 'C:\\Users\\ppzmis\\OneDrive - The University of Nottingham\\Documents\\Papers\\Charge\\ChargeProjectProcessedData\\torsional_spring_constant\\'
 filename = 'bluebead_torsion3.csv'
 data=np.loadtxt(pathname+filename,delimiter=',',encoding='utf-8')
 time = data[:,0]
 width = data[:,1]
 
-width = width[time > 15]
-time = time[time > 15]
+width = width[time > 12]
+time = time[time > 12]
 
 #plt.figure()
 #plt.plot(time,width)
